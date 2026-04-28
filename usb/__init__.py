0443793a36ef029dfbd3b7f25232821713b583d1
Fernando <gabrielfcassuncao@gmail.com>
2026-04-27 22:20:24 -0300

    deixando o app mais seguro

--- usb/__init__.py
+++ usb/__init__.py
@@ -3,17 +3,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+# importando a biblioteca os
+import os # serve de interação, é necessario
+
+# carregando a biblioteca de ambiente virtual
+from dotenv import load_dotenv
+
+# carregando o arquivo
+load_dotenv('.env') # esse arquivo pode ter qualquer nome
+
 # startando o aplicativo
 app = Flask(__name__) # ele vai iniciar o aplicativo com base nesse arquivo.
 
 # definindo o banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI') # acessando variavel do ambiente virtual
 
 # verificador do banco de dados 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # token de segurança
-app.config['SECRET_KEY'] = 'ADSFGIOEWQN90320T9832FENHV2W0E2'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') # acessando as variaveis do ambiente virtual
 
 # definindo a variavel do banco de dados
 db = SQLAlchemy(app)
